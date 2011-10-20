c1f0189f9616aef609542ef86cb99cb70b6d2528
William Ross <will@spanner.org>
2011-10-20 17:31:52 +0100

    empty rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poier::Application.config.secret_token = 'afca161df6e6f6ef7723f561d7ae44da0fa88a44a5f214e0fdac9c5d2e574a7f8c0bfb36a1238d20a451b0401cf8d381126e7cd4c5fb7322c490f08e0ed369f3'
